49253b8fa439b79d8fbc40ab76cceb79018732d3
Unnar Þór Axelsson <unnar3@gmail.com>
2015-03-16 14:46:03 +0100

    display mesh

--- include/PCLfunctions/cloud_generation.h
+++ include/PCLfunctions/cloud_generation.h
@@ -28,21 +28,70 @@ namespace EXX {
 		    cloud->points.resize (cloud->width * cloud->height);
 
 		    // Generate the data
-		    srand (time(NULL));
+		    int index = 0;
 		    for (int i = 0; i < cloud_width; i++)
 		    {
 		    	for (int j = 0; j < cloud_height; j++)
 		    	{
-					cloud->points[i].x = i*10;
-					cloud->points[i].y = j*10;
-					cloud->points[i].z = 1.0;
-					cloud->points[i].r = 255;
-					cloud->points[i].g = 50 + i*10;
-					cloud->points[i].b = 50;
+		    		double ind_i = i+1.0;
+		    		double ind_j = j+1.0;
+					cloud->points[index].x = ind_i*10.0;
+					cloud->points[index].y = ind_j*10.0;
+					cloud->points[index].z = 1.0;
+					cloud->points[index].r = 255;
+					cloud->points[index].g = 50 + ind_i*10;
+					cloud->points[index].b = 50;
+					index++;
 				}
 		    }
 
 		    return cloud;
 		}
+
+		pcl::PolygonMesh greedyProjectionTriangulation(PointCloudT::Ptr plane)
+			{
+			    pcl::PolygonMesh triangle;
+
+			    // Normal estimation*
+			    pcl::NormalEstimation<PointT, pcl::Normal> n;
+        		pcl::PointCloud<pcl::Normal>::Ptr normals (new pcl::PointCloud<pcl::Normal>);
+			    pcl::search::KdTree<PointT>::Ptr tree (new pcl::search::KdTree<PointT>);
+			    pcl::PointCloud<pcl::PointXYZRGBNormal>::Ptr cloud_with_normals (new pcl::PointCloud<pcl::PointXYZRGBNormal>);
+			    pcl::search::KdTree<pcl::PointXYZRGBNormal>::Ptr tree2 (new pcl::search::KdTree<pcl::PointXYZRGBNormal>);
+
+			    // Initialize objects
+			    pcl::GreedyProjectionTriangulation<pcl::PointXYZRGBNormal> gp3;
+
+			    // Set the maximum distance between connected points (maximum edge length)
+			    gp3.setSearchRadius (160);
+
+			    // Set typical values for the parameters
+			    gp3.setMu (1.0);
+			    gp3.setMaximumNearestNeighbors (100);
+			    gp3.setMaximumSurfaceAngle(M_PI/4); // 45 degrees
+			    gp3.setMinimumAngle(M_PI/18); // 10 degrees
+			    gp3.setMaximumAngle(2*M_PI/3); // 120 degrees
+			    gp3.setNormalConsistency(false);
+
+			    pcl::PolygonMesh triangles_planes;
+
+			    tree->setInputCloud (plane);
+			    n.setInputCloud (plane);
+			    n.setSearchMethod (tree);
+			    n.setKSearch (20);
+			    n.compute (*normals);
+
+			    // Concatenate the XYZ and normal fields*
+			    pcl::concatenateFields (*plane, *normals, *cloud_with_normals);
+
+			    tree2->setInputCloud (cloud_with_normals);
+
+			    // Get result
+			    gp3.setInputCloud (cloud_with_normals);
+			    gp3.setSearchMethod (tree2);
+			    gp3.reconstruct (triangle);
+
+			    return triangle;
+			}
 	}
 }
